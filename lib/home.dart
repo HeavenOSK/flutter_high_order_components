d3a8f61617e87f3a81accc2b9fa414f3e0f0c3cf
HeavenOSK <ryunosukeheaven.ba@gmail.com>
2019-01-21 21:21:01 +0900

    Refactor: Home with methods

--- lib/home.dart
+++ lib/home.dart
@@ -22,116 +22,132 @@ class Home extends StatelessWidget {
     );
   }
 
-  _buildSettingsList() {
+  Widget _buildSettingsList() {
     return ListView(
       children: <Widget>[
-        Container(
-          height: 70.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                'ユーザ名',
-                style: _LabelTextStyle,
-              ),
-              GestureDetector(
-                child: Icon(Icons.navigate_next),
-              ),
-            ],
-          ),
-          padding: EdgeInsets.symmetric(horizontal: 12.0),
-          decoration: BoxDecoration(
-            border: Border(
-              bottom: BorderSide(
-                width: 1.0,
-                color: _BorderColor,
-              ),
-            ),
-          ),
-        ),
-        Container(
-          height: 70.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                'お知らせ',
-                style: _LabelTextStyle,
-              ),
-              RawMaterialButton(
-                child: Text(
-                  'もっと見る',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w700,
-                  ),
-                ),
-                fillColor: Colors.blue,
-                onPressed: () {},
-              ),
-            ],
-          ),
-          padding: EdgeInsets.symmetric(horizontal: 12.0),
-          decoration: BoxDecoration(
-            border: Border(
-              bottom: BorderSide(
-                width: 1.0,
-                color: _BorderColor,
-              ),
-            ),
-          ),
-        ),
-        Container(
-          height: 70.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                'アプリ連携',
-                style: _LabelTextStyle,
-              ),
-              Switch(
-                value: false,
-                onChanged: (currentValue) {},
-              ),
-            ],
-          ),
-          padding: EdgeInsets.symmetric(horizontal: 12.0),
-          decoration: BoxDecoration(
-            border: Border(
-              bottom: BorderSide(
-                width: 1.0,
-                color: _BorderColor,
-              ),
-            ),
-          ),
-        ),
-        Container(
-          height: 70.0,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              Text(
-                'バージョン',
-                style: _LabelTextStyle,
-              ),
-              Text(
-                '1.0.0',
-                style: _InformationTextStyle,
-              ),
-            ],
-          ),
-          padding: EdgeInsets.symmetric(horizontal: 12.0),
-          decoration: BoxDecoration(
-            border: Border(
-              bottom: BorderSide(
-                width: 1.0,
-                color: _BorderColor,
-              ),
-            ),
-          ),
-        ),
+        _buildUserNameRow(),
+        _buildNotificationRow(),
+        _buildApplicationConnectionRow(),
+        _buildVersionNumberRow(),
       ],
+    );
+  }
+
+  Widget _buildVersionNumberRow() {
+    return Container(
+      height: 70.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            'バージョン',
+            style: _LabelTextStyle,
+          ),
+          Text(
+            '1.0.0',
+            style: _InformationTextStyle,
+          ),
+        ],
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 12.0),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 1.0,
+            color: _BorderColor,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildApplicationConnectionRow() {
+    return Container(
+      height: 70.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            'アプリ連携',
+            style: _LabelTextStyle,
+          ),
+          Switch(
+            value: false,
+            onChanged: (currentValue) {},
+          ),
+        ],
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 12.0),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 1.0,
+            color: _BorderColor,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildNotificationRow() {
+    return Container(
+      height: 70.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            'お知らせ',
+            style: _LabelTextStyle,
+          ),
+          RawMaterialButton(
+            child: Text(
+              'もっと見る',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+            fillColor: Colors.blue,
+            onPressed: () {},
+          ),
+        ],
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 12.0),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 1.0,
+            color: _BorderColor,
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildUserNameRow() {
+    return Container(
+      height: 70.0,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Text(
+            'ユーザ名',
+            style: _LabelTextStyle,
+          ),
+          GestureDetector(
+            child: Icon(Icons.navigate_next),
+          ),
+        ],
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 12.0),
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 1.0,
+            color: _BorderColor,
+          ),
+        ),
+      ),
     );
   }
 }
